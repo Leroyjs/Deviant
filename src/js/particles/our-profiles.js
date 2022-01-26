072ebb0d9780f0f5eabda6d3838b83c574676439
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-01-26 18:07:51 +0500

    profiles photo fix

--- src/js/particles/our-profiles.js
+++ src/js/particles/our-profiles.js
@@ -6,26 +6,55 @@ function ourProfiles() {
     '.our-profiles__item'
   );
 
-  !isTouchable && _ourProfilesMainWrapper.forEach(item => initItem(item));
-}
+  let ourProfilesWidth =
+    _ourProfilesMainWrapper[0].getBoundingClientRect().width - 40;
 
-function initItem(item) {
-  const _ourProfilesImgWrapper = item.querySelector(
-    '.our-profiles__photo-wrapper'
+  window.addEventListener(
+    'resize',
+    () =>
+      (ourProfilesWidth =
+        _ourProfilesMainWrapper[0].getBoundingClientRect().width - 40)
   );
 
-  item.addEventListener('mousemove', e => {
-    _ourProfilesImgWrapper.style.transform = `translate(calc(${
-      e.pageX + 20
-    }px), calc(${e.offsetY - 20}px - 100%))`;
-    _ourProfilesImgWrapper.classList.add('our-profiles__photo-wrapper_active');
-  });
+  !isTouchable && _ourProfilesMainWrapper.forEach(item => initItem(item));
 
-  item.addEventListener('mouseout', () => {
-    _ourProfilesImgWrapper.classList.remove(
-      'our-profiles__photo-wrapper_active'
+  function initItem(item) {
+    const _ourProfilesImgWrapper = item.querySelector(
+      '.our-profiles__photo-wrapper'
     );
-  });
+    const photo = item.querySelector('.our-profiles__photo');
+
+    item.addEventListener('mousemove', e => {
+      const photoRect = photo.getBoundingClientRect();
+      const photoRightOfMouse = photoRect.width < ourProfilesWidth - e.pageX;
+
+      if (photoRightOfMouse) {
+        setPositionOfBlock(_ourProfilesImgWrapper, e, 'right');
+      } else {
+        setPositionOfBlock(_ourProfilesImgWrapper, e, 'left');
+      }
+      _ourProfilesImgWrapper.classList.add(
+        'our-profiles__photo-wrapper_active'
+      );
+    });
+
+    item.addEventListener('mouseout', () => {
+      _ourProfilesImgWrapper.classList.remove(
+        'our-profiles__photo-wrapper_active'
+      );
+    });
+  }
+
+  function setPositionOfBlock(block, event, direction = 'right') {
+    const translateX =
+      direction === 'right'
+        ? `calc(${event.pageX + 20}px`
+        : `calc(${event.pageX - 20}px - 100%`;
+
+    block.style.transform = `translate(${translateX}), calc(${
+      event.offsetY - 20
+    }px - 100%))`;
+  }
 }
 
 export { ourProfiles };
