26b24809c1b5aad83f1cb3e2ebf485e28f2ba346
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-07-10 17:07:14 +0500

    form fetch script

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,24 +1,19 @@
-import axios from "axios";
-
 export const initForm = () => {
   const form = document.querySelector(".form-section__main");
 
   form.onsubmit = async (e) => {
     e.preventDefault();
 
-    axios({
-      method: "post",
-      url: "http://backoffice.deviant-studio.com/letters/inbox",
-      data: new FormData(form),
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then(function (response) {
-        //handle success
-        console.log(response);
-      })
-      .catch(function (response) {
-        //handle error
-        console.log(response);
-      });
+    const formdata = new FormData(form);
+
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+    };
+
+    fetch("https://backoffice.deviant-studio.com/letters/inbox", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   };
 };
