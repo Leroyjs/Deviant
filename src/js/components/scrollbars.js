595a6a9f8965f6f497acac5091a94bca9fa4070e
ITyugaev <igor.tyugaev@gmail.com>
2022-07-25 22:52:32 +0500

    Fix smooth scroll for mobile

--- src/js/components/scrollbars.js
+++ src/js/components/scrollbars.js
@@ -3,18 +3,29 @@ import Scrollbar from "smooth-scrollbar";
 import {noop} from "../utils/utils";
 
 export function initScrollbar(callback = noop, plugin) {
-    const scrollWrapperBlock = document.querySelector(".main");
-    const isTouchable = isTouchDevice();
+  const scrollWrapperBlock = document.querySelector(".main");
+  const isTouchable = isTouchDevice();
 
+  if (isTouchable) {
+    if (plugin) {
+      scrollWrapperBlock.classList.add('is-touch');
+      window.addEventListener('scroll', callback);
+      const pl = new plugin();
+      pl.onInit()
+    } else {
+      scrollWrapperBlock.addEventListener('scroll', callback);
+    }
+  } else {
     if (plugin) Scrollbar.use(plugin);
 
     const mainScrollbar = Scrollbar.init(scrollWrapperBlock, {
-        alwaysShowTracks: true,
-        damping: isTouchable ? 1 : 0.1,
+      alwaysShowTracks: true,
+      damping: isTouchable ? 1 : 0.1,
     });
 
     mainScrollbar.addListener(() => {
-        callback();
-        mainScrollbar.update();
+      callback();
+      mainScrollbar.update();
     });
+  }
 }
